58b49f597577f3b6a83285c3650aa424dcac1906
ASAZU TAIGA <taiga.asazu@icloud.com>
2020-09-21 22:18:02 +0900

    トップページのみプレレンダリング実装

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 module.exports = {
   transpileDependencies: ["vuetify"],
   configureWebpack: {
-    devtool: "source-map"
+    devtool: "source-map",
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, "dist"),
+        routes: ["/page/1"]
+      })
+    ]
   }
 };
