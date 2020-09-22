edb9a8aa9dc4b2790fa995928e26fe9b0870b128
ASAZU TAIGA <taiga.asazu@icloud.com>
2020-09-22 16:25:06 +0900

    PurgeCSS追加

---
+++ postcss.config.js
@@ -0,0 +1,37 @@
+const IN_PRODUCTION = process.env.NODE_ENV === "production";
+
+module.exports = {
+  plugins: [
+    IN_PRODUCTION &&
+      require("@fullhuman/postcss-purgecss")({
+        content: [`./public/**/*.html`, `./src/**/*.vue`],
+        defaultExtractor(content) {
+          const contentWithoutStyleBlocks = content.replace(
+            /<style[^]+?<\/style>/gi,
+            ""
+          );
+          return (
+            contentWithoutStyleBlocks.match(
+              /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+            ) || []
+          );
+        },
+        safelist: [
+          /-(leave|enter|appear)(|-(to|from|active))$/,
+          /^(?!(|.*?:)cursor-move).+-move$/,
+          /^router-link(|-exact)-active$/,
+          /data-v-.*/,
+          /^v-((?!application).)*$/,
+          /^theme--*/,
+          /.*-transition/,
+          /^justify-*/,
+          /^p*-[0-9]/,
+          /^m*-[0-9]/,
+          /^text--*/,
+          /--text$/,
+          /^row-*/,
+          /^col-*/
+        ]
+      })
+  ]
+};
